d1602ec43162153799d9bdbb1dc4690ebd5b94dd
jhar3ll <jharell.chambers@selu.edu>
2021-01-06 21:15:08 -0600

    backend setup 1

---
+++ db/migrate/20210107031153_create_plants.rb
@@ -0,0 +1,13 @@
+class CreatePlants < ActiveRecord::Migration[6.0]
+  def change
+    create_table :plants do |t|
+      t.string :name
+      t.string :date
+      t.string :water_frequency
+      t.string :sun_needed
+      t.boolean :alive
+
+      t.timestamps
+    end
+  end
+end
