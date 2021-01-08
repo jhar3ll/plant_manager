a251a36fcebe1d34f375c9e4ba21b0819e00ff52
jhar3ll <jharell.chambers@selu.edu>
2021-01-07 20:08:27 -0600

    form updates

--- plant_manager_client/src/components/PlantsForm.js
+++ plant_manager_client/src/components/PlantsForm.js
@@ -10,9 +10,17 @@ class PlantsForm extends Component {
         alive: 'yes'
     }
 
+    handleChange = event => {
+        const { name, value } = event.target 
+
+        this.setState({
+            [name]: value
+        })
+    }
+
     render() {
         return (
-           <form>
+           <form onSubmit={this.handleSubmit}>
                <label>Name:</label>
                <input type='text' value={this.state.name} onChange={this.handleChange} name='name'/>
                <br />
@@ -20,14 +28,28 @@ class PlantsForm extends Component {
                <input type='datetime-local' value={this.state.home_date} onChange={this.handleChange} name='home_date'/>
                <br />
                <label>How often to water?</label>
-               <input type='text' value={this.state.water_frequency} onChange={this.handleChange} name='water_frequency'/>
+               <select defaultValue="">
+                   <option hidden value=""></option>
+                   <option value={this.state.water_frequency} onChange={this.handleChange} name='water_frequency'>1x per day</option>
+                   <option value={this.state.water_frequency} onChange={this.handleChange} name='water_frequency'>2x per day</option>
+                   <option value={this.state.water_frequency} onChange={this.handleChange} name='water_frequency'>3x per day</option>
+                   <option value={this.state.water_frequency} onChange={this.handleChange} name='water_frequency'>4x per day</option>
+              </select>
                <br />
-               <label>How much sun is needed?</label>
-               <input type='text' value={this.state.sun_needed} onChange={this.handleChange} name='sun_needed'/>
+               <label>How much sun is needed per day?</label>
+               <select defaultValue="">
+                   <option hidden value=""></option>
+                   <option value={this.state.sun_needed} onChange={this.handleChange} name='sun_needed'>1 hour</option>
+                   <option value={this.state.sun_needed} onChange={this.handleChange} name='sun_needed'>2 hours</option>
+                   <option value={this.state.sun_needed} onChange={this.handleChange} name='sun_needed'>4 hours</option>
+                   <option value={this.state.sun_needed} onChange={this.handleChange} name='sun_needed'>6 hours</option>
+              </select>
                <br />
                <label>Is it alive? </label>
-               <select> 
-                   <option value={this.state.alive} onChange={this.handleChange} name='alive'>{this.state.alive} </option>
+               <select defaultValue="">
+                   <option hidden value=""></option>
+                   <option value={this.state.alive} onChange={this.handleChange} name='alive'>Yes</option>
+                   <option value={this.state.alive} onChange={this.handleChange} name='alive'>No</option>
               </select>
                <br />
                <input type='submit' value='Create Plant'/>
