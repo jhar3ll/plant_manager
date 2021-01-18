4199eed463db0b44f27ac4599c13092635e5c91a
jhar3ll <jharell.chambers@selu.edu>
2021-01-17 21:25:18 -0600

    status working

--- plant_manager_client/src/components/Plants.js
+++ plant_manager_client/src/components/Plants.js
@@ -5,13 +5,24 @@ import Countdown from './Countdown';
 
 const Plants = ({ plants }) => {
 
+const allPlants = (p) => {  
+        if (p.deprived_count === 0){
+            return 'good!'
+        } else if (p.deprived_count === 1){
+            return 'struggling!'
+        } else {
+            return 'dead :( '
+        }
+    }
+
+
     return (
         <div>
             {plants.map(plant => <ul>
                 <li key={plant.id}>
             <h3>
                 {plant.name} brought home on {plant.home_date}. Needs to be watered {plant.water_frequency} times per day. 
-                Currently! <Countdown plants={plant}/>
+                Currently {allPlants(plant)} <Countdown plants={plant}/>
                 </h3>
             </li>
             </ul>)}
